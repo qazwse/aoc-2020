3ab965601bd0e0f7e3db4c092518690a88a4bc5b
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-12-01 23:43:24 -0500

    Finished day 1, also init repository

---
+++ day1.lisp
@@ -0,0 +1,12 @@
+(ql:quickload '(:alexandria))
+
+(defparameter *day1-test* (mapcar #'parse-integer (uiop:read-file-lines "day1-test")))
+(defparameter *day1-input* (mapcar #'parse-integer (uiop:read-file-lines "day1-input")))
+
+(defun find-sum (list length sum)
+  (alexandria:map-permutations #'(lambda (x) (when (= sum (reduce #'+ x)) (return-from find-sum x))) list :length length))
+
+(find-sum *day1-test* 2 2020)
+(find-sum *day1-test* 3 2020)
+(find-sum *day1-input* 2 2020)
+(find-sum *day1-input* 3 2020)
