ff92eb4c106460e2fa1e67df1f80eba711590573
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-12-02 10:04:34 -0500

    Done part 2

--- day2.lisp
+++ day2.lisp
@@ -1,8 +1,7 @@
 ;;;; Day 2 - Password Philosophy
 ;;;; Part 1 - Find passwords that don't match criteria
 
-(ql:quickload '(:alexandria :cl-ppcre))
-(declaim (optimize (speed 0) (space 0) (debug 3)))
+(ql:quickload '(:cl-ppcre))
 
 (defparameter *test-input* (uiop:read-file-lines "day2-test"))
 (defparameter *problem-input* (uiop:read-file-lines "day2-input"))
@@ -20,7 +19,22 @@
          (c   (count char pw)))
     (and (>= c min) (<= c max))))
 
+(defun xor (pred-a pred-b)
+  (if (and pred-a pred-b)
+      nil
+      (or pred-a pred-b)))
+
+(defun valid-password-2? (criteria)
+  (let ((p1 (1- (getf criteria :min)))
+        (p2 (1- (getf criteria :max)))
+        (char (getf criteria :char))
+        (pw (getf criteria :password)))
+    (xor (equal char (char pw p1)) (equal char (char pw p2)))))
+
 
 ;; Part 1
 ;; 519
 (length (remove-if-not #'valid-password? (mapcar #'parse-input *problem-input*)))
+;; Part 2
+;; 708
+(length (remove-if-not #'valid-password-2? (mapcar #'parse-input *problem-input*)))
