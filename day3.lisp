7d115060d79323f177b16c15b77afeaafecb0d8b
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-12-03 10:36:35 -0500

    Clean up the loop

--- day3.lisp
+++ day3.lisp
@@ -12,9 +12,9 @@
          (width (length (first input)))
          (pos 0)
          (hits 0))
-   (loop :for row :in input :by height doing
+   (loop :for row :in input :by height :do
       (when (char= (char row pos) #\#)
-        (setf hits (1+ hits)))
+        (incf hits))
       (setf pos (mod (+ movement pos) width))
       :finally (return hits))))
 
@@ -23,9 +23,10 @@
 (day3-solver "day3-test" 5) ;-> 3
 (day3-solver "day3-test" 7) ;-> 4
 
-(*
- (day3-solver "day3-input" 1) ;-> 84
- (day3-solver "day3-input" 3) ;-> 195
- (day3-solver "day3-input" 5) ;-> 70
- (day3-solver "day3-input" 7) ;-> 70
- (day3-solver "day3-input" 1 #'cddr)) ;-> 47, 3772314000
+(time
+ (*
+  (day3-solver "day3-input" 1) ;-> 84
+  (day3-solver "day3-input" 3) ;-> 195
+  (day3-solver "day3-input" 5) ;-> 70
+  (day3-solver "day3-input" 7) ;-> 70
+  (day3-solver "day3-input" 1 #'cddr))) ;-> 47, 3772314000
