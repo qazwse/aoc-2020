223ce2fe2f9af88b77b5e4a4293a3710a927201c
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-12-03 13:54:51 -0500

    The cddr trick was clever, but using two arrays is more extensible

--- day3.lisp
+++ day3.lisp
@@ -1,32 +1,40 @@
 ;;;; Day 3 - TBD
 ;;;; I should really make a template containing code I'll probably need
 
-(ql:quickload '(:cl-ppcre :alexandria))
+(ql:quickload '(:iterate))
 (declaim (optimize (speed 0) (safety 0) (debug 3)))
 
-(defun read-input (filename)
-  (uiop:read-file-lines filename))
+(use-package :iterate)
 
-(defun day3-solver (filename movement &optional (height #'cdr))
-  (let* ((input (read-input filename))
-         (width (length (first input)))
-         (pos 0)
-         (hits 0))
-   (loop :for row :in input :by height :do
-      (when (char= (char row pos) #\#)
-        (incf hits))
-      (setf pos (mod (+ movement pos) width))
-      :finally (return hits))))
+(defconstant +input+ (coerce (uiop:read-file-lines "day3-input") 'vector))
+(defconstant +test+ (coerce (uiop:read-file-lines "day3-test") 'vector))
 
-(day3-solver "day3-test" 1) ;-> 2
-(day3-solver "day3-test" 3) ;-> 7
-(day3-solver "day3-test" 5) ;-> 3
-(day3-solver "day3-test" 7) ;-> 4
+(defun day3-solver-loop (input x &optional (y 1))
+  (let ((height (length input))
+        (width  (length (aref input 0))))
+    (loop :for row :from 0 :below height :by y
+          :for col = 0 :then (mod (+ col x) width)
+          :count (char= (char (aref input row) col) #\#))))
 
-(time
- (*
-  (day3-solver "day3-input" 1) ;-> 84
-  (day3-solver "day3-input" 3) ;-> 195
-  (day3-solver "day3-input" 5) ;-> 70
-  (day3-solver "day3-input" 7) ;-> 70
-  (day3-solver "day3-input" 1 #'cddr))) ;-> 47, 3772314000
+(defun day3-solver-iterate (input x &optional (y 1))
+  (let ((height (length input))
+        (width  (length (aref input 0))))
+    (iter
+      (for row from 0 below height by y)
+      (for col initially 0 then (mod (+ col x) width))
+      (counting (char= (char (aref input row) col) #\#)))))
+
+(day3-solver-loop +test+ 1) ;-> 2
+(day3-solver-loop +test+ 3) ;-> 7
+(day3-solver-loop +test+ 5) ;-> 3
+(day3-solver-loop +test+ 7) ;-> 4
+
+(day3-solver-loop +input+ 1) ;-> 84
+(day3-solver-loop +input+ 3) ;-> 195
+(day3-solver-loop +input+ 5) ;-> 70
+(day3-solver-loop +input+ 7) ;-> 70
+(day3-solver-loop +input+ 1 2) ;-> 47
+
+; 3772314000
+(reduce #'* (mapcar (lambda (x y) (day3-solver-loop +input+ x y)) '(1 3 5 7 1) '(1 1 1 1 2)))
+(reduce #'* (mapcar (lambda (x y) (day3-solver-iterate +input+ x y)) '(1 3 5 7 1) '(1 1 1 1 2)))
