a33cd9fb297c7e3d4d3597304a1d03e466f9d08e
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-12-02 23:15:29 -0500

    Day 3 start!

---
+++ day3.lisp
@@ -0,0 +1,10 @@
+;;;; Day 3 - TBD
+;;;; I should really make a template containing code I'll probably need
+
+(ql:quickload '(:cl-ppcre :alexandria))
+
+(defun read-input (filename)
+  (uiop:read-file-lines filename))
+
+(defun day3-solver ()
+  (let ((input (read-input "day3-test")))))
