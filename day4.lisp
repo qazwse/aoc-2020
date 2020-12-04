e2142194ffabb9be04cae5756cb3105851ac79e6
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-12-04 12:06:11 -0500

    Finish part 1

--- day4.lisp
+++ day4.lisp
@@ -1,11 +1,11 @@
 ;;;; Day 4
 
-(ql:quickload '(:alexandria :cl-ppcre :iterate))
+(ql:quickload '(:alexandria :cl-ppcre :iterate :str :binding-arrows))
 (use-package :iterate)
+(use-package :binding-arrows)
 (declaim (optimize (speed 0) (safety 0) (debug 3)))
 
 (defparameter *passport-flags* '("byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"))
-(defparameter *optional-flag* '("cid"))
 
 (defun parse-input (input)
   (iter
@@ -14,17 +14,24 @@
         (iter (for line = (pop input))
           (until (not line))
           (until (equal line ""))
-          (accumulate line :by (lambda (x y) (uiop:strcat x " " y)) :initial-value "")))))
+          (accumulate line :by (lambda (x y) (uiop:strcat x " " y)))))))
 
 (defun valid-passport (passport)
-  (let ((len (count #\  passport)))
-    (cond
-      ((= len 6) t)
-      ((and (= len 5) (not (find "cid" passport))) t)
-      (t nil))))
+  (->> (mapcar #'(lambda (key) (str:contains? key passport)) *passport-flags*)
+    (reduce #'(lambda (x y) (and x y)))))
 
 (defun day4-solver (filename)
-  (let ((lines (uiop:read-file-lines filename)))
-    (parse-input lines)))
+  (->> filename
+    uiop:read-file-lines
+    parse-input
+    (mapcar #'str:trim)
+    (count-if #'valid-passport)))
+
 
 (day4-solver "day4-input")
+
+(mapcar #'str:trim (day4-solver "day4-test"))
+
+(str:concat "asdf" "asdf")
+
+(reduce (lambda (x y) (and x y)) '(t nil t nil) :initial-value t)
