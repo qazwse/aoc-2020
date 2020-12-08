88c027eff39c2b413fd1d8b178e0cb91d63dc4ce
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-12-07 19:42:06 -0500

    Small error from refactoring

--- day7.lisp
+++ day7.lisp
@@ -5,32 +5,44 @@
   (:local-nicknames (:a :alexandria)))
 (in-package :day7)
 
-(defun make-bag (bag-info))
-
+(defun create-bags (bag-str)
+  (let ((bag-list '()))
+    (ppcre:do-register-groups ((#'parse-integer num) bag-str)
+        ("(\\d+) (\\w+ \\w+)" bag-str)
+      (push (list num bag-str) bag-list))
+    bag-list))
 
 (defun read-input (filename)
   (iter
-    (with bag-table = (make-hash-table))
+    (with bag-table = (make-hash-table :test #'equal))
     (for line in (uiop:read-file-lines filename))
     (for (first second) = (str:split " contain " line))
-    (for bag = (ppcre:scan-to-strings ""))
+    (for key = (ppcre:scan-to-strings "^\\w+ \\w+" first))
+    (setf (gethash key bag-table) (create-bags second))
+    (finally (return bag-table))))
 
-    (setf (gethash bag bag-table))))
+(defun dfs-p1 (index depth list search-term)
+  (iter (for (count colour) in list)
+    (when (equal colour search-term)
+      (return-from dfs-p1 t))
+    (collect (dfs-p1 index (1+ depth) (gethash colour index) search-term))))
 
-(ppcre:scan )
+(defun dfs-start-p1 (index search-term)
+  (iter (for (ele children) in-hashtable index)
+    (count (a:flatten (dfs-p1 index 1 children search-term)))))
 
-|=
-  (let ((bag-info (make-hash-table)))
-    (->> filename
-      uiop:read-file-line ; change to lines after testing
-      (str:split " contain ")
-      (setf (gethash))))
-=|
+(defun dfs-p2 (index depth list-term)
+  (let ((seq (gethash list-term index)))
+    (when (not seq)
+      (return-from dfs-p2 0))
+    (iter (for (count colour) in seq)
+      (summing (+ count (* count (dfs-p2 index (1+ depth) colour))) into bag-count)
+      (finally (return bag-count)))))
 
-(defun day7-solver (filename)
-  (->> filename
-    (read-input)))
+(defun day7-solver-p1 (filename)
+  (dfs-start-p1 (read-input filename) "shiny gold"))
 
+(defun day7-solver-p2 (filename)
+  (dfs-p2 (read-input filename) 0 "shiny gold"))
 
-;(day7-solver "day7-input")
-;(day7-solver "day7-test")
+;(day7-solver-p1 "day7-input")
