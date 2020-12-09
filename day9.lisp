220aa90d557078c2a6fe405cd24c4228112fabdb
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-12-09 13:47:36 -0500

    Finish day 9 :( I'm becoming so slow...

--- day9.lisp
+++ day9.lisp
@@ -6,8 +6,55 @@
 
 (in-package :day9)
 
-(defun day9-solver (filename)
-  (let ((raw-data (uiop:read-file-lines filename)))))
+(defun read-n (stream n)
+  (iter (repeat n)
+    (for i = (read stream))
+    (collect i at beginning)))
 
-;(day9-solver "day9-test")
-;(day9-solver "day9-input")
+(defun n-sum (list length sum)
+  (a:map-permutations
+    #'(lambda (x) (when (= sum (reduce #'+ x)) (return-from n-sum t)))
+    list :length length)
+  nil)
+
+(defun solve-p1 (filename preamble)
+  (with-open-file (in filename)
+    (iter
+      (for raw-data initially (read-n in preamble) then (cons i raw-data))
+      (for i = (read in nil))
+      (while i)
+      (unless invalid
+        (progn
+          (for cipher = (subseq raw-data 0 preamble))
+          (when (not (n-sum cipher 2 i)) (collect i into invalid))))
+      (finally (return (values (first invalid) (coerce (reverse raw-data) 'vector)))))))
+
+(defun p2-answer (data)
+  (print (reduce #'+ data))
+  (iter (for i in-vector data)
+    (maximize i into max)
+    (minimize i into min)
+    (finally (return (+ max min)))))
+
+(defun solve-p2 (data invalid)
+  (iter
+    (with sums = (make-hash-table :size (length data)))
+    (for n in-vector data with-index i)
+    (sum n into sum)
+    (for seen = (gethash (- sum invalid) sums))
+    (when (or seen (= invalid sum))
+      (finish))
+    (setf (gethash sum sums) i)
+    (finally (return (if seen
+                        (p2-answer (subseq data (1+ seen) (1+ i)))
+                        (p2-answer (subseq data 0 (1+ i))))))))
+
+
+(defun day9-solver (filename preamble)
+  (multiple-value-bind (invalid data) (solve-p1 filename preamble)
+    (format t "Part 1: Invalid number: ~A~%" invalid)
+    (format t "Part 2: Answer: ~A~%" (solve-p2 data invalid))))
+
+;(day9-solver "day9-test" 5)
+;(day9-solver "day9-input" 25)
+; (declaim (optimize (speed 3) (safety 0) (debug 0)))
