d13e4f4adb502dc16cbe41104fe87358d4bff9cf
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-12-10 10:59:06 -0500

    Did it the memoized way :)

--- day10.lisp
+++ day10.lisp
@@ -18,6 +18,19 @@
     ;(format t "~A ~A ~A~%" n prev-n diff)
     (finally (return (list ones twos threes)))))
 
+(defun p2-dfs (data window &optional (curr '(0)) (memos (make-hash-table :size 1000 :test #'eq)))
+  (when (not data)
+    (return-from p2-dfs 1))
+  (when (gethash (first curr) memos)
+    (return-from p2-dfs (gethash (first curr) memos)))
+  (iter
+    (for n = (pop data))
+    (while n)
+    (while (<= (- n (first curr)) window))
+    (summing (p2-dfs data window (cons n curr) memos) into c)
+    (finally
+     (setf (gethash (first curr) memos) c)
+     (return c))))
 
 (defun p2-solver (data)
   (iter (with dp = (make-hash-table))
@@ -36,8 +49,8 @@
          (sorted-data (sort int-data #'<))
          (sorted-data (append sorted-data (list (+ 3 (a:lastcar sorted-data))))))
     (format t "Part 1: ~A~%" (p1-solver sorted-data))
-    (format t "Part 2: ~A~%" (p2-solver sorted-data))))
+    (format t "~%Part 2: ~A~%" (p2-solver sorted-data))))
 
-;(day10-solver "day10-input") - 66,28
+;(time (day10-solver "day10-input")) - 66,28
 ;(day10-solver "day10-test")
 ;(day10-solver "day10-test2")
