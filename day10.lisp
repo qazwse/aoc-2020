4f80bc648b78f29a30c04deb8d5a92a1e6d617ba
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-12-10 01:13:27 -0500

    Day 10 done

--- day10.lisp
+++ day10.lisp
@@ -15,17 +15,29 @@
       (1 (counting n into ones))
       (2 (counting n into twos))
       (3 (counting n into threes)))
-    (format t "~A ~A ~A~%" n prev-n diff)
-    (finally (return (list ones twos (1+ threes))))))
+    ;(format t "~A ~A ~A~%" n prev-n diff)
+    (finally (return (list ones twos threes)))))
+
+
+(defun p2-solver (data)
+  (iter (with dp = (make-hash-table))
+    (initially (setf (gethash 0 dp) 1))
+    (for i in data)
+    (for first  = (gethash (- i 1) dp 0))
+    (for second = (gethash (- i 2) dp 0))
+    (for third  = (gethash (- i 3) dp 0))
+    (for sum    = (+ first second third))
+    (setf (gethash i dp) sum)
+    (finally (return (gethash (a:lastcar data) dp)))))
 
 (defun day10-solver (filename)
   (let* ((raw-data (uiop:read-file-lines filename))
          (int-data (mapcar #'parse-integer raw-data))
-         (sorted-data (sort int-data #'<)))
-    (format t "Part 1: ~A~%" (p1-solver sorted-data))))
+         (sorted-data (sort int-data #'<))
+         (sorted-data (append sorted-data (list (+ 3 (a:lastcar sorted-data))))))
+    (format t "Part 1: ~A~%" (p1-solver sorted-data))
+    (format t "Part 2: ~A~%" (p2-solver sorted-data))))
 
-
-
-;(day10-solver "day10-input")
+;(day10-solver "day10-input") - 66,28
 ;(day10-solver "day10-test")
 ;(day10-solver "day10-test2")
