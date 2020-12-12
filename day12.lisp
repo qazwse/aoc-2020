bcb7f4f02909cbc4650dd2a6a80c9dcd1063a070
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-12-12 13:43:04 -0500

    uiop was failing on very large files (> 10 000 000)

--- day12.lisp
+++ day12.lisp
@@ -1,8 +1,8 @@
-(defpackage :day12
-  (:use :cl :binding-arrows :iterate :trivia)
-  (:local-nicknames (:a :alexandria) (:re :ppcre)))
+(ql:quickload '(:alexandria :cl-ppcre :iterate :str :binding-arrows :trivia :series))
 
-(use-package :trivia.ppcre)
+(defpackage :day12
+  (:use :cl :binding-arrows :series :trivia)
+  (:local-nicknames (:a :alexandria) (:re :ppcre)))
 
 (in-package :day12)
 
@@ -15,75 +15,65 @@
 
 (defun p1-solver (data)
   (let ((heading #\E)
-        (v-dist 0)
-        (h-dist 0)
+        (dist (cons 0 0))
         (new '()))
-    (dolist (instr data new)
-      (destructuring-bind (code val) instr
+    (dolist (instr data)
+     (let ((code (aref instr 0))
+           (val (parse-integer (subseq instr 1))))
         (case code
           ((#\R #\L) (setf heading (get-new-heading heading code val)))
           (#\F (push (list heading val) new))
-          (otherwise (push (list code val) new)))))
-    (dolist (instr (reverse new))
+          (#\N (setf (cdr dist) (+ (cdr dist) val)))
+          (#\E (setf (car dist) (+ (car dist) val)))
+          (#\S (setf (cdr dist) (- (cdr dist) val)))
+          (#\W (setf (car dist) (- (car dist) val))))))
+    (dolist (instr new)
       (destructuring-bind (code val) instr
         (case code
-          (#\N (setf v-dist (+ v-dist val)))
-          (#\E (setf h-dist (+ h-dist val)))
-          (#\S (setf v-dist (- v-dist val)))
-          (#\W (setf h-dist (- h-dist val))))))
-    (+ (abs v-dist) (abs h-dist))))
+          (#\N (setf (cdr dist) (+ (cdr dist) val)))
+          (#\E (setf (car dist) (+ (car dist) val)))
+          (#\S (setf (cdr dist) (- (cdr dist) val)))
+          (#\W (setf (car dist) (- (car dist) val))))))
+    (+ (abs (car dist)) (abs (cdr dist)))))
 
-(defun rotate-x (dir rot x)
-  (let* ((headings (list #'+ #'+ #'- #'-)))
-    (if (char= #\R dir)
-        (apply (nth (1+ (mod (/ rot 90) 4)) headings) x)
-        (apply (nth (1+ (mod (/ rot -90) 4)) headings) x))))
-
-(defun rotate-xy (pt org dir theta)
-  (let* ((theta (if (char= dir #\L) (- theta) theta))
-         (rad (* pi (/ theta 180.0)))
+(defun rotate-xy (pt theta)
+  (let* ((rad (* pi (/ theta 180.0)))
          (x (car pt))
          (y (cdr pt))
-         (ox (car org))
-         (oy (cdr org))
-         (ad-x (- x ox))
-         (ad-y (- y oy))
          (cos-rad (cos rad))
          (sin-rad (sin rad)))
     (cons
-     (round (+ ox
-               (+ (* cos-rad ad-x)
-                  (* sin-rad ad-y))))
-     (round (+ oy
-               (+ (* (- sin-rad) ad-x)
-                  (* cos-rad ad-y)))))))
+     (round (+ (* cos-rad x)
+               (* sin-rad y)))
+     (round (+ (* (- sin-rad) x)
+               (* cos-rad y))))))
 
-(defun p2-solver (data)
-  (let ((v-dist 0)
-        (h-dist 0)
-        (wp (cons 10 1)))
-    (dolist (instr data)
-      (destructuring-bind (code val) instr
+(defun day12-solver (filename)
+  (let* ((data (uiop:read-file-lines filename)))
+    ;(format t "P1: ~A~%" (p1-solver data))
+    (format t "P2: ~A~%" (p2-solver data))))
+
+(defun p2-solver (filename)
+  (let ((dist (cons 0 0))
+        (wp   (cons 10 1)))
+    (iterate ((instr (scan-file filename #'read-line)))
+      (let ((code (aref instr 0))
+            (val (parse-integer (subseq instr 1))))
         (case code
-          ((#\R #\L) (setf wp (rotate-xy wp (cons 0 0) code val)))
-          (#\F (setf h-dist (+ h-dist (* (car wp) val)))
-               (setf v-dist (+ v-dist (* (cdr wp) val))))
+          (#\R (setf wp (rotate-xy wp val)))
+          (#\L (setf wp (rotate-xy wp (- val))))
+          (#\F (setf (car dist) (+ (car dist) (* (car wp) val)))
+               (setf (cdr dist) (+ (cdr dist) (* (cdr wp) val))))
           (#\N (setf (cdr wp) (+ (cdr wp) val)))
           (#\E (setf (car wp) (+ (car wp) val)))
           (#\S (setf (cdr wp) (- (cdr wp) val)))
           (#\W (setf (car wp) (- (car wp) val))))))
-    (+ (abs v-dist) (abs h-dist))))
+    (+ (abs (car dist)) (abs (cdr dist)))))
 
-(defun parse-data (data)
-  (->> data
-    (mapcar #'(lambda (s) (list (aref s 0) (parse-integer (subseq s 1)))))))
 
-(defun day12-solver (filename)
-  (let* ((data (uiop:read-file-lines filename))
-         (data (parse-data data)))
-    (format t "P1: ~A~%" (p1-solver data))
-    (format t "P2: ~A~%" (p2-solver data))))
+(time (p1-solver *data*))
+(time (p2-solver "day12-bb1"))
 
-(day12-solver "day12-input")
+;(time (day12-solver "day12-bb1"))
 
-(declaim (optimize (speed 0) (safety 0) (debug 3)))
+(declaim (optimize (speed 3) (safety 0) (debug 0)))
